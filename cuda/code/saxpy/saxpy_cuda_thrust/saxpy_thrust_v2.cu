379b77e00c79d8ceb00cd3d140375824787c3f87
Pierre Kestener <pierre.kestener@cea.fr>
2021-05-13 18:52:31 +0200

    mini update saxpy/thrust

--- cuda/code/saxpy/saxpy_cuda_thrust/saxpy_thrust_v2.cu
+++ cuda/code/saxpy/saxpy_cuda_thrust/saxpy_thrust_v2.cu
@@ -1,5 +1,5 @@
 /*
- * nvcc -O3 --ptxas-options=-v -gencode=arch=compute_20,code=sm_20 saxpy_thrust_v2.cu -o saxpy_thrust_v2
+ * nvcc -O3 --ptxas-options=-v -gencode=arch=compute_80,code=sm_80 saxpy_thrust_v2.cu -o saxpy_thrust_v2
  *
  */
 
